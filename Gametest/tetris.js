9c93be5316d807dbf4eba6cb0b5ece3e6b89ac7c
Megumism <philomegumi@gmail.com>
2020-02-15 20:16:16 +0800

    new folder

---
+++ Gametest/tetris.js
@@ -0,0 +1,10 @@
+let map = [];
+
+for (let i = 0; i < 24; i++) {
+    let temp = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    map.push(temp);
+}
+
+for (let i = 0; i < 24; i++) {
+    console.log(map[i]);
+}
